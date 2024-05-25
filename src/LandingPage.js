bffb7193efba89a5cadc3e556f809a680d0fc88c
Anne Do <doanneda@gmail.com>
2024-05-25 15:41:39 -0700

    log out button

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -5,13 +5,14 @@ import './LandingPage.css';
 import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
 
 const LandingPage = () => {
-  const { login, register } = useKindeAuth();
+  const { login, register, logout  } = useKindeAuth();
 
   return (
     <div className="landing-page">
       <div className="btn-container">
         <button onClick={login} className="button-64" type="button">Log In</button>
         <button onClick={register} className="button-64" type="button">Sign Up</button>
+        <button onClick={logout} className="button-64" type="button">Log Out</button>
       </div>
       <img src={sky} alt="sky" className="sky-img" />
       <img src={rocket} alt="rocket" className="rocket-img" />
