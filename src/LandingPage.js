b86d4f774dc2916bfa91c7a06349d9800082f28b
night-pianist <sweetkhiet907@gmail.com>
2024-05-25 11:30:20 -0700

    bleh

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -5,14 +5,12 @@ import './LandingPage.css';
 const LandingPage = () => {
     return ( 
         <div className="landing-page">
-            <div className="background-img">
-                <div className="btns">
-                    <button>login</button>
-                    <button>sign up</button>
-                </div>
-                <img src={sky} alt="sky" className="sky-img"/>
-                <img src={rocket} alt="rocket" className="rocket-img"/>
+            <div className="btns">
+                <button>login</button>
+                <button>sign up</button>
             </div>
+            <img src={sky} alt="sky" className="sky-img"/>
+            <img src={rocket} alt="rocket" className="rocket-img"/>
         </div>
      );
 }
