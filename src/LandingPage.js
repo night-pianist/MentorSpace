50c647843da002344d9916def82b9ae3cff26314
Anne Do <doanneda@gmail.com>
2024-05-25 14:05:22 -0700

    login

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,18 +1,22 @@
+import React from 'react';
 import rocket from './media/rocket.png';
 import sky from './media/sky.png';
 import './LandingPage.css';
+import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
 
 const LandingPage = () => {
-    return ( 
-        <div className="landing-page">
-            <div className="btn-container">
-                <button>login</button>
-                <button>sign up</button>
-            </div>
-            <img src={sky} alt="sky" className="sky-img"/>
-            <img src={rocket} alt="rocket" className="rocket-img"/>
-        </div>
-     );
+  const { login, register } = useKindeAuth();
+
+  return (
+    <div className="landing-page">
+      <div className="btn-container">
+        <button onClick={register} type="button">Sign Up</button>
+        <button onClick={login} type="button">Log In</button>
+      </div>
+      <img src={sky} alt="sky" className="sky-img" />
+      <img src={rocket} alt="rocket" className="rocket-img" />
+    </div>
+  );
 }
- 
+
 export default LandingPage;
