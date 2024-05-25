49f781c0aa5a778f25c2c54f3d8aadd31045c039
Anne Do <doanneda@gmail.com>
2024-05-25 14:55:20 -0700

    landing buttons ver 1

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -5,14 +5,13 @@ import './LandingPage.css';
 import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
 
 const LandingPage = () => {
-  const { login, register, logout } = useKindeAuth();
+  const { login, register } = useKindeAuth();
 
   return (
     <div className="landing-page">
       <div className="btn-container">
-        <button onClick={register} type="button">Sign Up</button>
-        <button onClick={login} type="button">Log In</button>
-        <button onClick={logout} type="button">Sign out</button>
+        <button onClick={register} className="button-64" type="button">Sign Up</button>
+        <button onClick={login} className="button-64" type="button">Log In</button>
       </div>
       <img src={sky} alt="sky" className="sky-img" />
       <img src={rocket} alt="rocket" className="rocket-img" />
