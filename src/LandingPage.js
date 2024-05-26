aeddad426fba62b5c0186d274e98e7b658527aae
Jasmine <jjasminew16@gmail.com>
2024-05-26 01:20:00 -0700

    styling landingpage

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -39,6 +39,12 @@ const LandingPage = () => {
         ) }
 
       </div>
+      <h1 className="heading">Launch your career to new heights</h1>
+      <h2 className="subtitle">Our mission is to empower women, non-binary, and underrepresented genders in engineering by pairing them with mentors who can help guide them through their careers, whether that be industry, research, or anything!</h2>
+      <h1 className="heading2">Mentorship and Guidance</h1>
+      <h2 className="subtitle2">Students are matched with mentors through their space of preferences, giving them a ranked list of mentors they can choose from.</h2>
+      <h1 className="heading3">Testimonials</h1>
+      <h2 className="subtitle3">"Especially in technical spaces, I like having communities that have some sort of focus/center on uplifting women because I find that it is just easier to mesh with people and that I am more comfortable with expressing myself unfilteredly when I am not as heavily dealing with those mental barriers of feeling out of place." - 1st Year College Student</h2>
       <img src={sky} alt="sky" className="sky-img" />
       <img src={rocket} alt="rocket" className="rocket-img" />
       <img src={circle} alt="circle" className="circle-img" />
