296105f455bc1e3313919e62c1530ec9b8150008
night-pianist <sweetkhiet907@gmail.com>
2024-05-25 20:32:17 -0700

    bleh

---
+++ backend/models/Mentee.js
@@ -0,0 +1,21 @@
+// models/Mentee.js
+const mongoose = require('mongoose');
+
+const menteeSchema = new mongoose.Schema({
+    name: String,
+    bio: String,
+    age: Number,
+    currentEducation: String,
+    contactPreference: String,
+    fieldOfStudy: String,
+    location: String,
+    careerAspirations: String,
+    interests: [String],
+    socialLinks: {
+        linkedIn: String,
+        instagram: String
+    },
+    role: String,
+});
+
+module.exports = mongoose.model('Mentee', menteeSchema);
